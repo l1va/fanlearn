73ac99fe4b38efe78254ad0a849f4968c37bab37
RunoVj <runodj@gmail.com>
2018-08-15 22:45:05 +0300

    Added simple movingto position command

--- fl_gazebo/test_moving.py
+++ fl_gazebo/test_moving.py
@@ -1,41 +1,164 @@
 #!/usr/bin/env python
 import rospy
-from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
+from pynput.keyboard import Key, Listener
 
-joint_names = ['joint_1','joint_2','joint_3','joint_4','joint_5','joint_6']
-#joint_names = ['shoulder_pan_joint', 'shoulder_lift_joint', 'elbow_joint', 
-#'wrist_1_joint', 'wrist_2_joint', 'wrist_3_joint']
+from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
+from moveit_msgs.srv import GetPositionIK, GetPositionIKRequest, \
+    GetPositionIKResponse
+from moveit_msgs.msg import MoveItErrorCodes
+
+import sys
+import copy
+import rospy
+import moveit_commander
+import moveit_msgs.msg
+import geometry_msgs.msg
+from math import pi
+from std_msgs.msg import String
+from moveit_commander.conversions import pose_to_list
+
+
+class Quartets:
+    def __init__(self, x=0, y=0, z=0, w=1):
+        self.x = x
+        self.y = y
+        self.z = z
+        self.w = w
+
+
+class Coordinates:
+    def __init__(self, x=0, y=0, z=0, q_x=0, q_y=0.5, q_z=0, q_w=0.5):
+        self.x = x
+        self.y = y
+        self.z = z
+        self.q = Quartets(q_x, q_y, q_z, q_w)
+
+    def __str__(self):
+        return '({} {} {}) - quartets: ({} {} {} {})'.format(self.x, self.y,
+                                                             self.z, self.q.x,
+                                                             self.q.y, self.q.z,
+                                                             self.q.w)
+
+
+joint_names = ['joint_1', 'joint_2', 'joint_3',
+               'joint_4', 'joint_5', 'joint_6']
+
+speed = 0.3
+time = 5
+
+goal = Coordinates()
 
 
 def move_to(pub, positions, velocities, times):
- 
     jt = JointTrajectory()
     jt.joint_names = joint_names
     jt.header.stamp = rospy.Time.now()
- 
+
     for (position, velocity, time) in zip(positions, velocities, times):
         jtp = JointTrajectoryPoint()
         jtp.positions = position
         jtp.velocities = velocity
         jtp.time_from_start = rospy.Duration(time)
         jt.points.append(jtp)
- 
+
     pub.publish(jt)
-    rospy.loginfo("%s: starting %.2f sec traj", "self.controller_name", times[-1])
+    rospy.loginfo("%s: starting %.2f sec traj", "self.controller_name",
+                  times[-1])
+
+def move_to_point(pub, joint_positions, speed, time):
+    jt = JointTrajectory()
+    jt.joint_names = joint_names
+    jt.header.stamp = rospy.Time.now()
+
+    jtp = JointTrajectoryPoint()
+    jtp.positions = joint_positions
+    jtp.velocities = [speed] * 6
+    jtp.time_from_start = rospy.Duration(time)
+    jt.points.append(jtp)
+
+    pub.publish(jt)
+    rospy.loginfo("%s: starting %.2f sec traj", "self.controller_name", time)
+
+
+def parse_position_resp(pos_resp):
+    if pos_resp.error_code.val == MoveItErrorCodes.SUCCESS:
+        print('OK')
+        return pos_resp.solution.joint_state.position
+    elif pos_resp.error_code.val == MoveItErrorCodes.NO_IK_SOLUTION:
+        print('No inverse kinematis solution')
+    else:
+        print('IK Error: {}'.format(pos_resp.error_code.val))
+
+
+def solve_ik(goal):
+    rospy.wait_for_service('compute_ik')
+    try:
+        print("try to solve ik...")
+        request = GetPositionIKRequest()
+        request.ik_request.group_name = "manipulator"
+        request.ik_request.ik_link_name = "link_6"
+        request.ik_request.attempts = 20
+        request.ik_request.pose_stamped.header.frame_id = "/base_link"
+        request.ik_request.pose_stamped.pose.position.x = goal.x
+        request.ik_request.pose_stamped.pose.position.y = goal.y
+        request.ik_request.pose_stamped.pose.position.z = goal.z
+        request.ik_request.pose_stamped.pose.orientation.x = goal.q.x
+        request.ik_request.pose_stamped.pose.orientation.y = goal.q.y
+        request.ik_request.pose_stamped.pose.orientation.z = goal.q.z
+        request.ik_request.pose_stamped.pose.orientation.w = goal.q.w
+        ik = rospy.ServiceProxy('compute_ik', GetPositionIK)
+        resp = ik(request)
+        return parse_position_resp(resp)
+
+    except rospy.ServiceException as e:
+        print('Service call failed - {}'.format(e))
 
 
 def main():
-	rospy.init_node('tester', anonymous=True)
+    rospy.init_node('tester', anonymous=True)
 
-	pub = rospy.Publisher("/arm_controller/command", JointTrajectory, queue_size=10)
-	rospy.sleep(0.5)
+    pub = rospy.Publisher("/arm_controller/command", JointTrajectory,
+                          queue_size=10)
+    rospy.sleep(0.5)
 
-	positions = [[0.1,0.1,0.1,0.1,0.1,0.1],
-	[0.2,0.2,0.2,0.2,0.2,0.2]]
-	velocities = [[1,1,1,1,1,1],[1,1,1,1,1,1]]
-	times = [9,19]
+    c = ''
+    while c != chr(27):
+        c = raw_input('input\n')
+        if c == 'd':
+            goal.x += 0.1
+        elif c == 'a':
+            goal.x -= 0.1
+        elif c == 'w':
+            goal.y += 0.1
+        elif c == 's':
+            goal.y -= 0.1
+        elif c == 'e':
+            goal.z += 0.1
+        elif c == 'q':
+            goal.z -= 0.1
+        elif c == 'D':
+            goal.q.x += 0.1
+        elif c == 'A':
+            goal.q.x -= 0.1
+        elif c == 'W':
+            goal.q.y += 0.1
+        elif c == 'S':
+            goal.q.y -= 0.1
+        elif c == 'E':
+            goal.q.z += 0.1
+        elif c == 'Q':
+            goal.q.z -= 0.1
+        elif c == 'C':
+            goal.q.w += 0.1
+        elif c == 'Z':
+            goal.q.w -= 0.1
 
-	move_to(pub, positions, velocities, times)
+
+        print(goal)
+
+        position = solve_ik(goal)
+        if position != None:
+            move_to_point(pub, position, speed, time)
 
 
 main()
